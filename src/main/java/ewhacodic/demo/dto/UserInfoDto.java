62ec9df011fdfe40e224964ddb67c1eb4df018f3
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-07-03 14:54:19 +0900

    Add files via upload

---
+++ src/main/java/ewhacodic/demo/dto/UserInfoDto.java
@@ -0,0 +1,13 @@
+package ewhacodic.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class UserInfoDto {
+    private String userName;
+    private String id;
+    private String password;
+
+    private String auth;
+}
---
+++ src/main/java/ewhacodic/demo/service/UserService.java
@@ -0,0 +1,34 @@
+package ewhacodic.demo.service;
+
+import ewhacodic.demo.domain.UserInfo;
+import ewhacodic.demo.dto.UserInfoDto;
+import ewhacodic.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public Long save(UserInfoDto infoDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        infoDto.setPassword(encoder.encode(infoDto.getPassword()));
+
+        return userRepository.save(UserInfo.builder()
+                .userName(infoDto.getUserName())
+                .auth(infoDto.getAuth())
+                .id(infoDto.getId())
+                .password(infoDto.getPassword()).build()).getCode();
+    }
+    // 시큐리티에서 지정한 서비스이기 때문에 이 메소드를 필수로 구현
+    @Override // 기본적인 반환 타입은 UserDetails, UserDetails를 상속받은 User로 반환 타입 지정 (자동으로 다운 캐스팅됨)
+    public UserInfo loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new UsernameNotFoundException((userName)));
+    }
+}
