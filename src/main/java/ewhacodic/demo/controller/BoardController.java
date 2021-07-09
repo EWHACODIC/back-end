c76525d3dae7be3233b99f5f9eea50cc1ea936f1
JangBomi <bonita9931@gmail.com>
2021-07-10 04:53:04 +0900

    feat- 제목으로 검색 기능 추가

--- src/main/java/ewhacodic/demo/controller/BoardController.java
+++ src/main/java/ewhacodic/demo/controller/BoardController.java
@@ -1,27 +1,35 @@
 package ewhacodic.demo.controller;
 
+import ewhacodic.demo.domain.Board;
+import ewhacodic.demo.domain.BoardComment;
+import ewhacodic.demo.dto.BoardCommentDto;
 import ewhacodic.demo.dto.BoardDto;
+import ewhacodic.demo.dto.BoardListDto;
 import ewhacodic.demo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping(value="/codic/community")
+@RequestMapping("/api/post")
 public class BoardController {
-    private BoardService boardService;
 
     @Autowired
+    private BoardService boardService;
+
     public BoardController(BoardService boardService){
         this.boardService = boardService;
     }
 
-    //전체 게시글 목록 가져오기
-    @GetMapping(value="/get")
-    public Iterable<BoardDto> list(){
-        return boardService.getBoardList(null);
-    }
-
+//    //전체 게시글 목록 가져오기
+//    @GetMapping(value="/get")
+//    public Iterable<BoardDto> list(){
+//        return boardService.getBoardList(null);
+//    }
+//
     //추천순 게시글 정렬
     @GetMapping(value="/order/recommend")
     public List<BoardDto> orderByRecommend(){
@@ -46,36 +54,88 @@ public class BoardController {
         return boardService.getBoardList("reply");
     }
 
-    //게시글 및 댓글 가져오기
+    //4. 게시글 하나 가져오기
     @GetMapping(value="/{postId}")
     public BoardDto detail(@PathVariable("postId") Long postId){
-        BoardDto boardDto = boardService.getPost(postId);
+        BoardDto boardDto = boardService.getPostOnly(postId);
         return boardDto;
     }
 
-    //게시글 작성
-    @PostMapping(value="/newPost")
-    public void write(BoardDto boardDto){
-        boardService.savePost(boardDto);
+    //4-1. 특정 게시글의 댓글 조회
+    @GetMapping("/{postId}/comment")
+    public List<BoardComment> getBoardComment(@PathVariable("postId") Long postId) {
+        return boardService.getCommentByPostId(postId);
     }
 
-    //게시글 수정
-    @PutMapping(value="/edit/{postId}")
-    public void update(@PathVariable("postId") Long id){
-        BoardDto boardDto = boardService.getPost(id);
-        boardService.savePost(boardDto);
+    //4-2. 특정 게시글 한번에 조회 -> 명세에 없음 + 엔드포인트 새로 구성
+    @GetMapping("post2/{postId}")
+    public Board getBoardPostAndComment(@PathVariable("postId") Long postId) {
+        return boardService.getPostAndComment(postId);
     }
 
-    //게시글 삭제
+    // 5. 게시글 목록 조회
+    @GetMapping("/list")
+    public List<BoardListDto> getBoardList() {
+        return boardService.getBoardListDto();
+    }
+
+
+    //6. 게시글 작성
+    @PostMapping("/new")
+    public ResponseEntity<String> write(@RequestBody BoardDto boardDto){
+        System.out.println("controller");
+        boardService.savePost(boardDto);
+        return ResponseEntity.ok("ok");
+    }
+
+    //7. 게시글 수정
+    @PatchMapping(value="/edit")
+    public ResponseEntity<String> update(@RequestBody Board board){
+        boardService.updatePost(board);
+        return ResponseEntity.ok("ok");
+    }
+
+    //8. 게시글 삭제
     @DeleteMapping(value="/{postId}")
-    public void delete(@PathVariable("postId") Long id){
+    public ResponseEntity<String> delete(@PathVariable("postId") Long id){
         boardService.deletePost(id);
+        return ResponseEntity.ok("ok");
     }
 
-    //게시글 추천
-    @GetMapping(value="/{postId}/recommend")
-    public void updateRecommend(@PathVariable("postId") Long id){
-        BoardDto boardDto = boardService.getPost(id);
-        boardDto.toEntity().updateRecommend();
+    //9. 게시글 추천수 증가
+    @PatchMapping(value="/{postId}/recommend")
+    public ResponseEntity<String> updateRecommend(@PathVariable("postId") Long id){
+        boardService.updateBoardRecommend(id);
+
+        return ResponseEntity.ok("ok");
     }
+
+    //10. 댓글 작성
+    @PostMapping("/{postId}/comment")
+    public ResponseEntity<String> saveComment(@PathVariable Long postId, @RequestBody BoardCommentDto boardCommentDto) {
+        boardService.saveComment(boardCommentDto, postId);
+        return ResponseEntity.ok("ok");
+    }
+
+    //11. 댓글 수정
+    @PutMapping("/{postId}/comment/{commentId}")
+    public ResponseEntity<String> updateComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody BoardCommentDto boardCommentDto) {
+        boardService.updateComment(postId, commentId, boardCommentDto);
+        return ResponseEntity.ok("ok");
+    }
+
+    //12. 댓글 삭제
+    @DeleteMapping("/{postId}/comment/{commentId}")
+    public ResponseEntity<String> deleteComment(@PathVariable Long postId, @PathVariable Long commentId) {
+        boardService.deleteComment(commentId, postId);
+        return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/list/key")
+    public List<BoardListDto> getBoardListByKeyWord(@RequestParam String keyword) {
+        return boardService.searchPosts(keyword);
+    }
+
+
+
 }
--- src/main/java/ewhacodic/demo/repository/BoardRepository.java
+++ src/main/java/ewhacodic/demo/repository/BoardRepository.java
@@ -3,25 +3,26 @@ package ewhacodic.demo.repository;
 import ewhacodic.demo.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    //게시판별 조회
-    @Query("select u From Post")
-    List<Board> findPosts();
+    List<Board> findByTitleContaining(String keyword);
 
-    @Query("select u From Post order by u.recommend")
-    List<Board> OrderByRecommend();
-
-    @Query("select u From Post order by u.view")
-    List<Board> OrderByView();
-
-    @Query("select u From Post order by u.id")
-    List<Board> OrderByLatest();
-
-    @Query("select u From Post order by u.reply")
-    List<Board> OrderByReply();
+//    @Query("select u From Board order by u.recommend")
+//    List<Board> OrderByRecommend();
+//
+//    //@Query("select u From Board order by u.view")
+//    List<Board> OrderByView();
+//
+//    //@Query("select u From Board order by u.id")
+//    List<Board> OrderByLatest();
+//
+//    //@Query("select u From Board order by u.reply")
+//    List<Board> OrderByReply();
 
 }
 
